3506b3521d7f31882a1aa0dfa382578b28beee4c
Erin Xu <erinxu2003@gmail.com>
2022-06-17 23:04:33 -0500

    set up new home page

---
+++ src/components/Shelf.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import '../styles/Shelf.scss';
+import Tile from './Tile';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import useDrag from '../components/Drag';
+import wood from '../media/wood.png';
+
+type scrollVisibilityApiType = React.ContextType<typeof VisibilityContext>;
+type item = {
+    name: string, 
+    location: string, 
+    date: string, 
+    time: string, 
+    restrict: string[], 
+    person: string, 
+    contact: string, 
+    category: string
+}
+
+
+const Shelf = (
+    {name, items} :
+    {name: string; items: item[]}
+) => {
+    return (
+        <div>
+            <p className='shelfName'>{name}</p>
+            <div className='tiles'>
+                {items.map((item) => {
+                    return (
+                        <div className='tile'> 
+                            <Tile name={item.name} location={item.location} date={item.date} time={item.time} restrict={item.restrict} person={item.person} contact={item.contact} category={item.category}/>
+                        </div>
+                    )
+                })}
+            </div>
+            <img src={wood} alt='shelf' className='shelf'/>
+        </div>
+    )
+}
+
+export default Shelf
+
+// const elemPrefix = "test";
+// const getId = (index: number) => `${elemPrefix}${index}`;
+
+// const getItems = () =>
+//   Array(20)
+//     .fill(0)
+//     .map((_, ind) => ({ id: getId(ind) }));
+
+// function App() {
+//   const [items] = React.useState(getItems);
+
+//   // NOTE: for drag by mouse
+//   const { dragStart, dragStop, dragMove, dragging } = useDrag();
+//   const handleDrag = ({ scrollContainer }: scrollVisibilityApiType) => (
+//     ev: React.MouseEvent
+//   ) =>
+//     dragMove(ev, (posDiff) => {
+//       if (scrollContainer.current) {
+//         scrollContainer.current.scrollLeft += posDiff;
+//       }
+//     });
+
+//   const [selected, setSelected] = React.useState<string>("");
+//   const handleItemClick = (itemId: string) => () => {
+//     if (dragging) {
+//       return false;
+//     }
+//     setSelected(selected !== itemId ? itemId : "");
+//   };
+
+//   return (
+//     <>
+//       <Header />
+//       <div className="example" style={{ paddingTop: "100px" }}>
+//         <div onMouseLeave={dragStop}>
+//           <ScrollMenu
+//             // LeftArrow={LeftArrow}
+//             // RightArrow={RightArrow}
+//             // onWheel={onWheel}
+//             onMouseDown={() => dragStart}
+//             onMouseUp={() => dragStop}
+//             onMouseMove={handleDrag}
+//           >
+//             {items.map(({ id }) => (
+//               <Card
+//                 title={id}
+//                 itemId={id} // NOTE: itemId is required for track items
+//                 key={id}
+//                 onClick={handleItemClick(id)}
+//                 selected={id === selected}
+//               />
+//             ))}
+//           </ScrollMenu>
+//         </div>
+//         <Footer />
+//       </div>
+//     </>
+//   );
+// }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import '../styles/Home.scss';
+import Shelf from '../components/Shelf';
+import Tile from '../components/Tile';
+import plant from '../media/plant.png';
+
+const Home = () => {
+
+    let example = [
+        {
+            name: 'name', 
+            location: 'location', 
+            date: 'date', 
+            time: 'time', 
+            restrict: ['nut', 'gluten', 'dairy'], 
+            person: 'person', 
+            contact: 'contact', 
+            category: 'meal'
+        },
+        {
+            name: 'name', 
+            location: 'location', 
+            date: 'date', 
+            time: 'time', 
+            restrict: ['nut', 'gluten', 'dairy'], 
+            person: 'person', 
+            contact: 'contact', 
+            category: 'meal'
+        },
+        {
+            name: 'name', 
+            location: 'location', 
+            date: 'date', 
+            time: 'time', 
+            restrict: ['nut', 'gluten', 'dairy'], 
+            person: 'person', 
+            contact: 'contact', 
+            category: 'meal'
+        },
+    ]
+
+    return (
+        <div>
+            <img src={plant} alt='plant'/>
+            <Shelf name='Students' items={example}/>
+            <Shelf name='Dining Halls' items={example}/>
+            <Shelf name='Restaurants' items={example}/>
+        </div>
+    )
+}
+
+export default Home
